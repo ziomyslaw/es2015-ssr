002714fa61e8b2bc7f2c485b3c04e435fce385c7
ziomyslaw <ziomyslaw@gmail.com>
2016-09-19 16:36:27 +0200

    router duplication
    Why the router (with definition of home only) is duplicated here? I'm asking as there is also in master/both/routes.jsx. Thanks!

--- imports/app/router.jsx
+++ imports/app/router.jsx
@@ -5,6 +5,7 @@ import BlogLayout from './layouts/blog.jsx';
 import PostList from './containers/post_list';
 import PostPage from './containers/post_page';
 
+/* remove the file? */ 
 FlowRouter.route("/", {
   name: "home",
   action() {
